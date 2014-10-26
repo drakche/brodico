aebb89d39eb37ef995eaee1366ffc31f174b072a
Dragan Stojanović <drakche.69@gmail.com>
2014-10-26 19:03:03 +0100

    fixed the title and the reference gallery

--- sites/all/themes/business/js/script.js
+++ sites/all/themes/business/js/script.js
@@ -1,5 +1,5 @@
 jQuery(document).ready(function () {
-    jQuery('.about-us-title').wrap('<a href="o-nama"></a>');
+    //jQuery('.about-us-title').wrap('<a href="o-nama"></a>');
     jQuery('.field-item').addClass('image-inline');
     jQuery('.sb-image.sb-gallery.sb-gallery-gallery').addClass('image-inline');
 });
